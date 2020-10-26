af234fa58ee71c3fc11d043f5e530b472645277c
luwis93choi <luwis93choi@hotmail.com>
2020-10-26 19:24:38 +0900

    Added MSE Loss Function graph with broken y axis

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from torch.autograd import Variable
 from torchsummaryX import summary
 
 import datetime
+import numpy as np
+from matplotlib import pyplot as plt
 
 normalize = transforms.Normalize(
     #mean=[121.50361069 / 127., 122.37611083 / 127., 121.25987563 / 127.],
@@ -37,19 +39,48 @@ deepvo_model.train()
 deepvo_model.training = True
 
 train_epoch = 3
+train_sequence = ['01']
+#train_sequence=['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
+test_sequence = ['01']
 
 train_loader = torch.utils.data.DataLoader(voDataLoader(img_dataset_path='/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/dataset/sequences',
                                                         pose_dataset_path='/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/data_odometry_poses/dataset/poses',
                                                         transform=preprocess,
                                                         test=False,
-                                                        train_sequence=['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
-                                                        test_sequence=['01']),
+                                                        train_sequence=train_sequence,
+                                                        test_sequence=test_sequence),
                                                         batch_size=1, shuffle=True, drop_last=True)
 
 criterion = torch.nn.MSELoss()
 optimizer = optim.SGD(deepvo_model.parameters(), lr=0.0001)
 
 summary(deepvo_model, Variable(torch.zeros((1, 6, 384, 1280)).to(PROCESSOR)))
+
+train_plot_color = plt.cm.get_cmap('rainbow', len(train_sequence))
+train_plot_x = 0
+
+### Plotting graph setup with broken y-axis ######################################
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(20, 8))
+ax1.set_ylim(2, 30)
+ax2.set_ylim(0, 1.5)
+
+ax1.spines['bottom'].set_visible(False)
+ax2.spines['top'].set_visible(False)
+
+ax1.xaxis.tick_top()
+ax1.tick_params(labeltop=False)
+ax2.xaxis.tick_bottom()
+
+d = .015    # how big to make the diagonal lines in axes coordinates
+            # arguments to pass to plot, just so we don't keep repeating them
+kwargs = dict(transform=ax1.transAxes, color='k', clip_on=False)
+ax1.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
+ax1.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
+
+kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
+ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
+ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
+################################################################################
 
 for epoch in range(train_epoch):
 
@@ -73,6 +104,19 @@ for epoch in range(train_epoch):
         optimizer.step()
 
         print('Batch : {} / Loss : {}'.format(batch_idx, loss))
+        
+        ax1.plot(train_plot_x, loss.item(), c=train_plot_color(train_loader.dataset.sequence_idx), marker='o')
+        ax2.plot(train_plot_x, loss.item(), c=train_plot_color(train_loader.dataset.sequence_idx), marker='o')
+
+        ax1.set_title('DeepVO Training with KITTI\nTraining Sequence ' + str(train_sequence))
+        ax2.set_xlabel('Training Length')
+        ax2.set_ylabel('MSELoss')
+
+        #plt.pause(0.0001)
+        #plt.show(block=False)
+        train_plot_x += 1
+
+    plt.savefig('./Training Results ' + str(datetime.datetime.now()) + '.png')
 
     train_loader.dataset.reset_loader()
 
