72ffc43018e7cf3ec741724b519fe0cdd31e7224
luwis93choi <luwis93choi@hotmail.com>
2020-11-09 14:48:54 +0900

    DeepVO Model Trainer Added / Removed legacy from main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from deepvoNet import DeepVONet
 from dataloader import voDataLoader
 
+from model_trainer import trainer
+
 import torch
 import torch.optim as optim
 from torchvision import transforms
@@ -11,6 +13,14 @@ from torchsummaryX import summary
 import datetime
 import numpy as np
 from matplotlib import pyplot as plt
+
+img_dataset_path = '/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/dataset/sequences'
+pose_dataset_path = '/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/data_odometry_poses/dataset/poses'
+
+train_epoch = 2
+train_sequence = ['01']
+#train_sequence=['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
+test_sequence = ['01']
 
 normalize = transforms.Normalize(
     #mean=[121.50361069 / 127., 122.37611083 / 127., 121.25987563 / 127.],
@@ -25,125 +35,11 @@ preprocess = transforms.Compose([
     normalize
 ])
 
-# Load main processing unit for neural network
-PROCESSOR = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+deepvo_trainer = trainer(use_cuda=True,
+                         loader_preprocess_param=preprocess,
+                         img_dataset_path=img_dataset_path,
+                         pose_dataset_path=pose_dataset_path,
+                         train_epoch=2, train_sequence=train_sequence, train_batch=1,
+                         plot_batch=False, plot_epoch=True)
 
-deepvo_model = DeepVONet()
-deepvo_model.to(PROCESSOR)
-
-if str(PROCESSOR) == 'cuda:0':
-    deepvo_model.use_cuda = True
-    deepvo_model.reset_hidden_states(size=1, zero=True)
-
-deepvo_model.train()
-deepvo_model.training = True
-
-train_epoch = 2
-train_sequence = ['01']
-#train_sequence=['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
-test_sequence = ['01']
-
-train_loader = torch.utils.data.DataLoader(voDataLoader(img_dataset_path='/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/dataset/sequences',
-                                                        pose_dataset_path='/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/data_odometry_poses/dataset/poses',
-                                                        transform=preprocess,
-                                                        sequence=train_sequence),
-                                                        batch_size=1, shuffle=True, drop_last=True)
-
-criterion = torch.nn.MSELoss()
-optimizer = optim.SGD(deepvo_model.parameters(), lr=0.0001)
-
-summary(deepvo_model, Variable(torch.zeros((1, 6, 384, 1280)).to(PROCESSOR)))
-
-train_plot_color = plt.cm.get_cmap('rainbow', len(train_sequence))
-train_plot_x = 0
-
-### Plotting graph setup with broken y-axis ######################################
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(20, 8))
-ax1.set_ylim(2, 30)
-ax2.set_ylim(0, 1.5)
-
-ax1.spines['bottom'].set_visible(False)
-ax2.spines['top'].set_visible(False)
-
-ax1.xaxis.tick_top()
-ax1.tick_params(labeltop=False)
-ax2.xaxis.tick_bottom()
-
-d = .015    # how big to make the diagonal lines in axes coordinates
-            # arguments to pass to plot, just so we don't keep repeating them
-kwargs = dict(transform=ax1.transAxes, color='k', clip_on=False)
-ax1.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
-ax1.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
-
-kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
-ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
-ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
-################################################################################
-
-training_loss = []
-
-for epoch in range(train_epoch):
-
-    print('[EPOCH] : {}'.format(epoch))
-
-    loss_sum = 0.0
-
-    for batch_idx, (prev_current_img, prev_current_odom) in enumerate(train_loader):
-
-        prev_current_img = Variable(prev_current_img.to(PROCESSOR))
-        prev_current_odom = Variable(prev_current_odom.to(PROCESSOR))
-
-        estimated_odom = Variable(torch.zeros(prev_current_odom.shape))
-
-        if train_loader.dataset.sequence_change == True:
-
-            # Sequence has changed LSTM reset
-            print('[Sequence Change] LSTM Reset')
-
-            deepvo_model.reset_hidden_states(size=1, zero=True)
-
-        optimizer.zero_grad()
-        
-        estimated_odom = deepvo_model(prev_current_img)
-        deepvo_model.reset_hidden_states(size=1, zero=False)
-
-        loss = criterion(estimated_odom, prev_current_odom.float())
-
-        loss.backward()
-        optimizer.step()
-
-        print('[EPOCH {}] Batch : {} / Loss : {}'.format(epoch, batch_idx, loss))
-    
-        ax1.plot(train_plot_x, loss.item(), c=train_plot_color(train_loader.dataset.sequence_idx), marker='o')
-        ax2.plot(train_plot_x, loss.item(), c=train_plot_color(train_loader.dataset.sequence_idx), marker='o')
-
-        ax1.set_title('DeepVO Training with KITTI [MSE Loss at each batch]\nTraining Sequence ' + str(train_sequence))
-        ax2.set_xlabel('Training Length')
-        ax2.set_ylabel('MSELoss')
-        
-        train_plot_x += 1
-
-        #plt.pause(0.0001)
-        #plt.show(block=False)
-
-        loss_sum += loss.item()
-
-    training_loss.append(loss_sum / len(train_loader))
-
-    plt.savefig('./Training Results ' + str(datetime.datetime.now()) + '.png')
-
-    print('[Epoch {} Complete] Loader Reset'.format(epoch))
-    train_loader.dataset.reset_loader()
-
-    print('[Epoch {} Complete] LSTM Reset'.format(epoch))
-    deepvo_model.reset_hidden_states(size=1, zero=True)
-
-plt.clf()
-plt.figure(figsize=(20, 8))
-plt.plot(range(train_epoch), training_loss, 'bo-')
-plt.title('DeepVO Training with KITTI [Average MSE Loss]\nTraining Sequence ' + str(train_sequence))
-plt.xlabel('Training Length')
-plt.ylabel('MSELoss')
-plt.savefig('./Training Results ' + str(datetime.datetime.now()) + '.png')
-
-torch.save(deepvo_model, './DeepVO_' + str(datetime.datetime.now()) + '.pth')
+deepvo_trainer.train()
--- model_trainer.py
+++ model_trainer.py
@@ -16,62 +16,150 @@ class trainer():
 
     def __init__(self, use_cuda=True, 
                        loader_preprocess_param=transforms.Compose([]), 
-                       train_epoch=1,
-                       train_sequence=['01'],
+                       img_dataset_path='', pose_dataset_path='',
+                       train_epoch=1, train_sequence=['01'], train_batch=1,
                        plot_batch=False, plot_epoch=True):
+
+        self.use_cuda = use_cuda
+
+        self.img_dataset_path = img_dataset_path
+        self.pose_dataset_path = pose_dataset_path
+
+        self.train_epoch = train_epoch
+        self.train_sequence = train_sequence
+        self.train_batch = train_batch
+        
+        self.plot_batch = plot_batch
+        self.plot_epoch = plot_epoch
 
         if use_cuda == True:        
             # Load main processing unit for neural network
-            PROCESSOR = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+            self.PROCESSOR = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-        deepvo_model = DeepVONet()
-        deepvo_model.to(PROCESSOR)
+        self.deepvo_model = DeepVONet()
+        self.deepvo_model.to(self.PROCESSOR)
 
 
-        if str(PROCESSOR) == 'cuda:0':
-            deepvo_model.use_cuda = True
-            deepvo_model.reset_hidden_states(size=1, zero=True)
+        if str(self.PROCESSOR) == 'cuda:0':
+            self.deepvo_model.use_cuda = True
+            self.deepvo_model.reset_hidden_states(size=1, zero=True)
 
-        deepvo_model.train()
-        deepvo_model.training = True
+        self.deepvo_model.train()
+        self.deepvo_model.training = True
 
-        train_loader = torch.utils.data.DataLoader(voDataLoader(img_dataset_path='/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/dataset/sequences',
-                                                                pose_dataset_path='/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/data_odometry_poses/dataset/poses',
-                                                                transform=loader_preprocess_param,
-                                                                sequence=train_sequence),
-                                                                batch_size=1, shuffle=True, drop_last=True)
+        self.train_loader = torch.utils.data.DataLoader(voDataLoader(img_dataset_path=self.img_dataset_path,
+                                                                     pose_dataset_path=self.pose_dataset_path,
+                                                                     transform=loader_preprocess_param,
+                                                                     sequence=train_sequence),
+                                                                     batch_size=self.train_batch, shuffle=True, drop_last=True)
 
-        criterion = torch.nn.MSELoss()
-        optimizer = optim.SGD(deepvo_model.parameters(), lr=0.0001)
+        self.criterion = torch.nn.MSELoss()
+        self.optimizer = optim.SGD(self.deepvo_model.parameters(), lr=0.0001)
 
-        summary(deepvo_model, Variable(torch.zeros((1, 6, 384, 1280)).to(PROCESSOR)))
+        summary(self.deepvo_model, Variable(torch.zeros((1, 6, 384, 1280)).to(self.PROCESSOR)))
 
-        train_plot_color = plt.cm.get_cmap('rainbow', len(train_sequence))
-        train_plot_x = 0
-
-        draw_broken_yaxis = False
-
-        if plot_batch == True:
+        # Plotting batch error graph
+        if self.plot_batch == True:
+            
+            self.train_plot_color = plt.cm.get_cmap('rainbow', len(train_sequence))
+            self.train_plot_x = 0
 
             ### Plotting graph setup with broken y-axis ######################################
-            fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(20, 8))
-            ax1.set_ylim(2, 30)
-            ax2.set_ylim(0, 1.5)
+            fig, (self.ax1, self.ax2) = plt.subplots(2, 1, sharex=True, figsize=(20, 8))
+            self.ax1.set_ylim(2, 30)
+            self.ax2.set_ylim(0, 1.5)
 
-            ax1.spines['bottom'].set_visible(False)
-            ax2.spines['top'].set_visible(False)
+            self.ax1.spines['bottom'].set_visible(False)
+            self.ax2.spines['top'].set_visible(False)
 
-            ax1.xaxis.tick_top()
-            ax1.tick_params(labeltop=False)
-            ax2.xaxis.tick_bottom()
+            self.ax1.xaxis.tick_top()
+            self.ax1.tick_params(labeltop=False)
+            self.ax2.xaxis.tick_bottom()
 
             d = .015    # how big to make the diagonal lines in axes coordinates
                         # arguments to pass to plot, just so we don't keep repeating them
-            kwargs = dict(transform=ax1.transAxes, color='k', clip_on=False)
-            ax1.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
-            ax1.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
+            kwargs = dict(transform=self.ax1.transAxes, color='k', clip_on=False)
+            self.ax1.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
+            self.ax1.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
 
-            kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
-            ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
-            ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
+            kwargs.update(transform=self.ax2.transAxes)  # switch to the bottom axes
+            self.ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
+            self.ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
             ################################################################################
+
+    def train(self):
+
+        training_loss = []
+
+        for epoch in range(self.train_epoch):
+
+            print('[EPOCH] : {}'.format(epoch))
+
+            loss_sum = 0.0
+
+            for batch_idx, (prev_current_img, prev_current_odom) in enumerate(self.train_loader):
+
+                if self.use_cuda == True:
+                    prev_current_img = Variable(prev_current_img.to(self.PROCESSOR))
+                    prev_current_odom = Variable(prev_current_odom.to(self.PROCESSOR))
+
+                    estimated_odom = Variable(torch.zeros(prev_current_odom.shape))
+
+                if self.train_loader.dataset.sequence_change == True:
+
+                    # Sequence has changed LSTM reset
+                    print('[Sequence Change] LSTM Reset')
+
+                    self.deepvo_model.reset_hidden_states(size=1, zero=True)
+
+                self.optimizer.zero_grad()
+                
+                estimated_odom = self.deepvo_model(prev_current_img)
+                self.deepvo_model.reset_hidden_states(size=1, zero=False)
+
+                loss = self.criterion(estimated_odom, prev_current_odom.float())
+
+                loss.backward()
+                self.optimizer.step()
+
+                print('[EPOCH {}] Batch : {} / Loss : {}'.format(epoch, batch_idx, loss))
+                    
+                # Plotting batch error graph
+                if self.plot_batch == True:
+                    self.ax1.plot(train_plot_x, loss.item(), c=self.train_plot_color(self.train_loader.dataset.sequence_idx), marker='o')
+                    self.ax2.plot(train_plot_x, loss.item(), c=self.train_plot_color(self.train_loader.dataset.sequence_idx), marker='o')
+
+                    self.ax1.set_title('DeepVO Training with KITTI [MSE Loss at each batch]\nTraining Sequence ' + str(train_sequence))
+                    self.ax2.set_xlabel('Training Length')
+                    self.ax2.set_ylabel('MSELoss')
+                    
+                    self.train_plot_x += 1
+
+                loss_sum += loss.item()
+
+            training_loss.append(loss_sum / len(self.train_loader))
+
+            # Save batch error graph
+            if self.plot_batch == True:
+                plt.savefig('./Training Results ' + str(datetime.datetime.now()) + '.png')
+
+            print('[Epoch {} Complete] Loader Reset'.format(epoch))
+            self.train_loader.dataset.reset_loader()
+
+            print('[Epoch {} Complete] LSTM Reset'.format(epoch))
+            self.deepvo_model.reset_hidden_states(size=1, zero=True)
+
+        # Plotting average loss on each epoch
+        if self.plot_epoch == True:
+            plt.clf()
+            plt.figure(figsize=(20, 8))
+            plt.plot(range(self.train_epoch), training_loss, 'bo-')
+            plt.title('DeepVO Training with KITTI [Average MSE Loss]\nTraining Sequence ' + str(self.train_sequence))
+            plt.xlabel('Training Length')
+            plt.ylabel('MSELoss')
+            plt.savefig('./Training Results ' + str(datetime.datetime.now()) + '.png')
+
+        torch.save(self.deepvo_model, './DeepVO_' + str(datetime.datetime.now()) + '.pth')
+
+        return self.deepvo_model, training_loss
+
